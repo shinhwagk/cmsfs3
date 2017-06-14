f7a3d84df6feaf499e80f80f987b06f1c53d71e6
gk <shanghai_sl@hotmail.com>
2017-06-14 15:52:20 +0800

    test

--- monitors/disk_space/src/test/java/DiskSpaceTest.java
+++ monitors/disk_space/src/test/java/DiskSpaceTest.java
@@ -7,15 +7,15 @@ import org.monitor.cmsfs.kafka.KafkaLocalServer;
  * Created by zhangxu on 2017/6/13.
  */
 public class DiskSpaceTest {
-    public DiskSpaceTest() {
-        new Thread(() -> KafkaLocalServer.apply(true).start()).start();
-    }
-
-    @Test
-    public void test() throws InterruptedException {
-        Producer producerThread = new Producer(ServiceProperties.TOPIC, true);
-        producerThread.start();
-        Thread.sleep(10001110);
-        System.out.println("aaa");
-    }
+//    public DiskSpaceTest() {
+//        new Thread(() -> KafkaLocalServer.apply(true).start()).start();
+//    }
+//
+//    @Test
+//    public void test() throws InterruptedException {
+//        Producer producerThread = new Producer(ServiceProperties.TOPIC, true);
+//        producerThread.start();
+//        Thread.sleep(10001110);
+//        System.out.println("aaa");
+//    }
 }
