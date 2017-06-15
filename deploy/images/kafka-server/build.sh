11f6bac2b45fc455897775f276bd9cce9260445a
gk <shanghai_sl@hotmail.com>
2017-06-15 14:22:32 +0800

    test

--- deploy/images/kafka-server/build.sh
+++ deploy/images/kafka-server/build.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-SCRIPT_DIR=`dirname $0`;
-BUILD_DIR="/tmp/$(date +%s%N)";
-
-mkidr $BUILD_DIR && cd $BUILD_DIR;
-wget http://apache.fayea.com/kafka/0.10.2.0/kafka_2.12-0.10.2.0.tgz
-
 docker build -t cmsfs3/kafka-server .
