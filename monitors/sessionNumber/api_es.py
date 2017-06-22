bb947053e9f4e76fda4673b8bca1724457eae718
gk <shanghai_sl@hotmail.com>
2017-06-22 18:41:33 +0800

    save

--- monitors/sessionNumber/api_es.py
+++ monitors/sessionNumber/api_es.py
@@ -2,7 +2,7 @@ import json
 import http.client
 
 def sendElasticsearch(_index, _type, content):
-      headers = {"Content-type": "application/json; charset=utf-8"}
+      headers = {"Content-type": "application/json; charset=utf-8","Authorization":"Basic ZWxhc3RpYzpjaGFuZ2VtZQ=="}
       conn = http.client.HTTPConnection("elasticsearch.cmsfs.org", 9200)
       url = "/%s/%s" % (_index, _type)
       conn.request("POST", url, json.dumps(content), headers)
